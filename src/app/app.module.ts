3e839124c96a39bffadc24758a9e1bfa8a705654
sergio <sergiolufi95@gmail.com>
2017-08-24 12:46:59 -0600

    primer uso de node

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientsComponent,
+    SalesComponent,
+    InventoryComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
