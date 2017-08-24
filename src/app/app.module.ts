9876fb8bfc9f9222f1df0f11db98d47beab14988
sergio <sergiolufi95@gmail.com>
2017-08-24 13:49:44 -0600

    header y declaracion de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//esto fue agregado
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ClientsComponent } from './components/clients/clients.component';
 import { SalesComponent } from './components/sales/sales.component';
@@ -8,6 +9,14 @@ import { InventoryComponent } from './components/inventory/inventory.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+
+var roots = [
+  { path: 'sales', component: SalesComponent},
+  { path: 'clients', component: ClientsComponent},
+  { path: 'inventory', component: InventoryComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -19,8 +28,10 @@ import { HomeComponent } from './components/home/home.component';
     FooterComponent,
     HomeComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(roots)
   ],
   providers: [],
   bootstrap: [AppComponent]
